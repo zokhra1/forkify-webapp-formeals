e0a7dc44353db3aafb16a2b08075197ea375f96e
zokhra1 <abiyevazokhra@gmail.com>
2023-05-04 21:45:13 +0400

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -123,6 +123,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the application!');
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
   recipeView.addHandlerUpdateServings(controlServings);
@@ -130,5 +134,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 init();
