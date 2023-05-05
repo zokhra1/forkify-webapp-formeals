d5a9f9470cc9bd77ced652836d7e457d127c8ccc
zokhra1 <abiyevazokhra@gmail.com>
2023-05-05 23:17:26 +0400

    new

--- src/js/controller.js
+++ src/js/controller.js
@@ -9,12 +9,13 @@ import paginationView from './views/paginationView.js';
 import bookmarksView from './views/bookmarksView.js';
 import addRecipeView from './views/addRecipeView.js';
 
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
+// import 'core-js/stable';
+// import 'regenerator-runtime/runtime';
+// import { async } from 'regenerator-runtime';
 
-if (module.hot) {
-  module.hot.accept();
-}
+// if (module.hot) {
+//   module.hot.accept();
+// }
 
 ///////////////////////////////////////
 
--- src/js/views/helpers.js
+++ src/js/views/helpers.js
@@ -1,3 +1,4 @@
+import 'regenerator-runtime/runtime';
 import { async } from 'regenerator-runtime';
 import { TIMEOUT_SEC } from '../config.js';
 
