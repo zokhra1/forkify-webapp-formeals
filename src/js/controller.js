d8ea48fa17d20244592f8fbbd9a1e680270d0976
zokhra1 <abiyevazokhra@gmail.com>
2023-05-05 20:06:58 +0400

    newer

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,7 +11,6 @@ import addRecipeView from './views/addRecipeView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-import { async } from 'regenerator-runtime';
 
 if (module.hot) {
   module.hot.accept();
--- src/js/model.js
+++ src/js/model.js
@@ -1,4 +1,3 @@
-import { async } from 'regenerator-runtime';
 import { API_URL, RES_PER_PAGE, KEY } from './config.js';
 // import { getJSON, sendJSON } from './views/helpers.js';
 import { AJAX } from './views/helpers.js';
