ec9fff4013ffbf51ad842bfde49b2e710605cf99
zokhra1 <abiyevazokhra@gmail.com>
2023-05-05 13:46:04 +0400

    new

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -97,9 +97,7 @@ class RecipeView extends View {
         <div class="recipe__ingredients">
           <h2 class="heading--2">Recipe ingredients</h2>
           <ul class="recipe__ingredient-list">
-          ${this._data.ingredients
-            .map(this._generateMarkupIngredients)
-            .join('')}
+          ${this._data.ingredients.map(this._generateMarkupIngredient).join('')}
           
         </div>
 
