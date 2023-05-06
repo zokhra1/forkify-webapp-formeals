75dd0af74019a6a20e3e60b128bb4a914258d1b5
zokhra1 <abiyevazokhra@gmail.com>
2023-05-06 18:12:47 +0400

    new

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -18,7 +18,7 @@ class RecipeView extends View {
     this._parentElement.addEventListener('click', function (e) {
       const btn = e.target.closest('.btn--update-servings');
       if (!btn) return;
-      const { updateTo } = +btn.dataset;
+      const updateTo = +btn.dataset.updateTo;
       if (+updateTo > 0) handler(+updateTo);
     });
   }
